6dc3c5702717b4fbe59dccf4c8dfa6784447851d
System Administrator <root@usuariop33.fi.upm.es>
2018-06-12 16:43:30 +0200

    RML virtualizer; v1.0; Cimmino 180612 1643

---
+++ src/test/java/rmlvirtualizer/RMLVirtualizerTest.java
@@ -0,0 +1,74 @@
+package rmlvirtualizer;
+
+import org.eclipse.rdf4j.rio.RDFFormat;
+import org.junit.Assert;
+import org.junit.Test;
+
+
+
+/**
+ * Created by Andrea Cimmino on 12/06/18
+ */
+public class RMLVirtualizerTest {
+
+
+	/**
+     * Test case 1: Virtualize JSON data
+     */
+    @Test
+    public void testAdditionAllPositive() {
+    		// init
+    		String mappingContent = "@prefix rr: <http://www.w3.org/ns/r2rml#>.\n" + 
+    				"@prefix rml: <http://semweb.mmlab.be/ns/rml#>.\n" + 
+    				"@prefix ql: <http://semweb.mmlab.be/ns/ql#>.\n" + 
+    				"@prefix transit: <http://vocab.org/transit/terms/>.\n" + 
+    				"@prefix xsd: <http://www.w3.org/2001/XMLSchema#>.\n" + 
+    				"@prefix wgs84_pos: <http://www.w3.org/2003/01/geo/wgs84_pos#>.\n" + 
+    				"\n" + 
+    				"<#AirportMapping>\n" + 
+    				"  rml:logicalSource [\n" + 
+    				"    rml:source \"./src/test/resources/test1-RMLVirtualizer/example.csv\" ;\n" + 
+    				"    rml:referenceFormulation ql:CSV \n" + 
+    				"  ];\n" + 
+    				"  rr:subjectMap [\n" + 
+    				"    rr:template \"http://airport.example.com/{id}\";\n" + 
+    				"    rr:class transit:Stop \n" + 
+    				"  ];\n" + 
+    				"\n" + 
+    				"  rr:predicateObjectMap [\n" + 
+    				"    rr:predicate transit:route;\n" + 
+    				"    rr:objectMap [\n" + 
+    				"      rml:reference \"stop\";\n" + 
+    				"      rr:datatype xsd:int\n" + 
+    				"      ]\n" + 
+    				"    ];\n" + 
+    				"\n" + 
+    				"  rr:predicateObjectMap [\n" + 
+    				"    rr:predicate wgs84_pos:lat;\n" + 
+    				"    rr:objectMap [\n" + 
+    				"      rml:reference \"latitude\"\n" + 
+    				"    ]\n" + 
+    				"  ];\n" + 
+    				"\n" + 
+    				"  rr:predicateObjectMap [\n" + 
+    				"    rr:predicate wgs84_pos:long;\n" + 
+    				"    rr:objectMap [\n" + 
+    				"      rml:reference \"longitude\"\n" + 
+    				"    ]\n" + 
+    				"  ].";
+    		
+    		String baseIRI = "http://pegaso.com/example#";
+    		RDFFormat outputFormat= RDFFormat.N3;
+    		// virtualize
+    		RMLVirtualizer virtualizer = new RMLVirtualizer();
+    		String virtualizedRDF =  virtualizer.virtualize(mappingContent, baseIRI, outputFormat).trim();
+    		// gold std
+        String desiredResult = "<http://airport.example.com/6523> a <http://vocab.org/transit/terms/Stop> ;\n" + 
+        		"	<http://www.w3.org/2003/01/geo/wgs84_pos#long> \"4.484444\" ;\n" + 
+        		"	<http://www.w3.org/2003/01/geo/wgs84_pos#lat> \"50.901389\" ;\n" + 
+        		"	<http://vocab.org/transit/terms/route> \"25\"^^<http://www.w3.org/2001/XMLSchema#int> .";
+      
+        Assert.assertEquals(virtualizedRDF.replace(" ", ""), desiredResult.replace(" ", ""));
+    }
+	
+}
